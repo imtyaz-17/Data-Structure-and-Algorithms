7ca3bcea5894323edb0940162a48f82d9521cb57
imtyaz-17 <imtyazit17017@gmail.com>
2022-11-13 15:59:31 +0600

    missing and repeating number

--- Arrays/Medium/3_Find_the_missing_and_repeating_number_by_mark_the_visited_places.cpp
+++ Arrays/Medium/3_Find_the_missing_and_repeating_number_by_mark_the_visited_places.cpp
@@ -3,3 +3,33 @@
 
 //Use elements as Index and mark the visited places
 //Time Complexity: O(n) , Auxiliary Space: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+void printMissing_RepeatingElements(int arr[], int sz){
+    cout << "The repeating element is ";
+    for (int  i = 0; i < sz; i++)
+    {
+        if (arr[abs(arr[i])-1]>0)
+        {
+            arr[abs(arr[i])-1]= - arr[abs(arr[i])-1];
+        }else{
+            cout<<abs(arr[i])<<endl;
+        }
+    }
+     cout << "and the missing element is ";
+     for (int i = 0; i < sz; i++)
+     {
+        if(arr[i]>0)
+            cout<<(i+1);
+     }
+     
+    
+}
+int main()
+{
+    int arr[] = { 7, 3, 4, 5, 5, 6, 2 };
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    printMissing_RepeatingElements(arr, sz);
+}
+ 
